c57e9ce7225a58a16d923d4a8f5c27ed7e03f9a0
Georgi Kamacharov <gkamacharov@gmail.com>
2020-04-14 10:09:04 -0400

    update context

--- gkama.graph.ql.data/CountryContext.cs
+++ gkama.graph.ql.data/CountryContext.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using System.Text;
+
+using Microsoft.EntityFrameworkCore;
 
 namespace gkama.graph.ql.data
 {
@@ -16,40 +19,59 @@ namespace gkama.graph.ql.data
         {
             modelBuilder.Entity<Country>(e =>
             {
-                e.HasKey(p => p.geoname_id);
+                e.HasKey(x => x.geoname_id);
 
-                e.Property(p => p.geoname_id).IsRequired();
-                e.Property(p => p.code).IsRequired();
+                e.Property(x => x.geoname_id).HasColumnName("geoname_id").IsRequired();
+                e.Property(x => x.code).HasColumnName("code").HasMaxLength(2).IsRequired();
+                e.Property(x => x.name).HasColumnName("name").HasMaxLength(100).IsRequired();
+                e.Property(x => x.iso_numeric).HasColumnName("iso_numeric");
+                e.Property(x => x.continent).HasColumnName("continent").HasMaxLength(2).IsRequired();
+                e.Property(x => x.continent_name).HasColumnName("continent_name").HasMaxLength(30).IsRequired();
+                e.Property(x => x.capital).HasColumnName("capital").HasMaxLength(50).IsRequired();
+                e.Property(x => x.population).HasColumnName("population").IsRequired();
+                e.Property(x => x.currency_code).HasColumnName("currency_code").HasMaxLength(3).IsRequired();
 
-                e.Property(p => p.continent).HasMaxLength(2);
-                e.Property(p => p.currency_code).HasMaxLength(3);
-
-                e.HasMany(p => p.neighbour_countries)
+                e.HasMany(x => x.neighbour_countries)
                     .WithOne()
-                    .HasForeignKey(p => p.country_geoname_id)
+                    .HasForeignKey(x => x.country_geoname_id)
+                    .OnDelete(DeleteBehavior.Cascade)
                     .IsRequired();
 
-                e.HasOne(p => p.postal_codes)
+                e.HasOne(x => x.postal_codes)
                     .WithOne()
-                    .HasForeignKey<CountryPostalCode>(p => p.code)
-                    .HasPrincipalKey<Country>(p => p.code)
+                    .HasForeignKey<CountryPostalCode>(x => x.code)
+                    .HasPrincipalKey<Country>(x => x.code)
+                    .OnDelete(DeleteBehavior.Cascade)
                     .IsRequired();
             });
 
             modelBuilder.Entity<CountryNeighbour>(e =>
             {
-                e.HasKey(p => p.geoname_id);
+                e.HasKey(x => x.geoname_id);
 
-                e.HasOne(p => p.postal_codes)
+                e.HasIndex(x => x.country_geoname_id);
+
+                e.Property(x => x.geoname_id).HasColumnName("geoname_id").IsRequired();
+                e.Property(x => x.code).HasColumnName("code").HasMaxLength(2).IsRequired();
+                e.Property(x => x.name).HasColumnName("name").HasMaxLength(50).IsRequired();
+                e.Property(x => x.country_geoname_id).HasColumnName("country_geoname_id").IsRequired();
+
+                e.HasOne(x => x.postal_codes)
                     .WithOne()
-                    .HasForeignKey<CountryPostalCode>(p => p.code)
-                    .HasPrincipalKey<CountryNeighbour>(p => p.code)
+                    .HasForeignKey<CountryPostalCode>(x => x.code)
+                    .HasPrincipalKey<CountryNeighbour>(x => x.code)
+                    .OnDelete(DeleteBehavior.Cascade)
                     .IsRequired();
             });
 
             modelBuilder.Entity<CountryPostalCode>(e =>
             {
-                e.HasKey(p => p.code);
+                e.HasKey(x => x.code);
+
+                e.Property(x => x.code).HasColumnName("code").HasMaxLength(2).IsRequired();
+                e.Property(x => x.num_postal_codes).HasColumnName("num_postal_codes").IsRequired();
+                e.Property(x => x.min_postal_code).HasColumnName("min_postal_code").IsRequired();
+                e.Property(x => x.max_postal_code).HasColumnName("max_postal_code").IsRequired();
             });
         }
     }
